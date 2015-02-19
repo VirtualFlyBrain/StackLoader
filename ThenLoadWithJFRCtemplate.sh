524379dcd796a8d198930d7cff3ab43ca338b4ca
Robbie1977 <robbie.court@gmail.com>
2015-02-19 18:09:20 +0000

    syntax fix

--- ThenLoadWithJFRCtemplate.sh
+++ ThenLoadWithJFRCtemplate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Assign convert and these stacks against the JFRC adult main brain template (001)
+# Run this script to convert and load files listed in linkData.tsv to /disk/data/VFB/IMAGE_DATA/VFB... 
+# Note: All files will be removed after processing.
+# Then apply these stacks against the JFRC adult main brain template (001)
 export template=001
-expost voxelSize='-x0.622088 -y0.622088 -z0.622088'
+export voxelSize='-x0.622088 -y0.622088 -z0.622088'
 ./src/convert.sh
 ./src/setTemplate.sh
