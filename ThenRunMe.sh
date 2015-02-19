3121c94c2d841327023f41d4201c7212c69b1829
Robbie1977 <robbie.court@gmail.com>
2015-02-19 16:28:01 +0000

    nrrd installed in 2.6 not 2.7

--- ThenRunMe.sh
+++ ThenRunMe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run this script to convert and load files listed in linkData.tsv to /disk/data/VFB/IMAGE_DATA/VFB... 
 # Note: All files will be removed after processing.
-source /partition/bocian/VFBTools/python-modules-2.7/bin/activate
+source /partition/bocian/VFBTools/python-modules-2.6/bin/activate
 ./src/convert.sh
 echo Done.
