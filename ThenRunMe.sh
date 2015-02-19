730375a5231b85a93e820b1f4aea23247c83c1dc
Jenkins CI Server <jenkins@karenin.inf.ed.ac.uk>
2015-02-19 13:10:14 +0000

    initial framework

---
+++ ThenRunMe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run this script to convert and load files listed in linkData.tsv to /disk/data/VFB/IMAGE_DATA/VFB... 
+# Note: All files will be removed after processing.
+./src/convert.sh
+echo Done.
