2c7ab6f69bb88581c89899a366abe41d6fbbc9f9
Jenkins CI Server <jenkins@karenin.inf.ed.ac.uk>
2015-02-21 23:12:55 +0000

    updated checks

--- src/checkFolders.sh
+++ src/checkFolders.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Checks all image folders and logs where a file type is missing or broken into missingFiles.tsv
 echo 'Checking files for...'
-echo reference$'\t'data.jso$'\t'thumbnail.png$'\t'index.html$'\t'volume.nrrd > missingFiles.tsv
+echo reference$'\t'data.jso$'\t'thumbnail.png$'\t'index.html$'\t'volume.nrrd > history/missingFiles.tsv
+echo reference$'\t'data.jso$'\t'thumbnail.png$'\t'index.html$'\t'volume.nrrd > history/fullFilesList.tsv
 for folder in /partition/karenin/VFB/IMAGE_DATA/VFB/i/*/*/
 do 
 ref=$(echo ${folder} | replace '/' '' | replace 'partitionkareninVFBIMAGE_DATAVFBi' 'VFB_')$'\t'
@@ -11,6 +12,7 @@ if [ ! -f ${folder}data.jso ]; then result=$(echo ${result},1); else result=$(ec
 if [ ! -f ${folder}thumbnail.png ]; then result=$(echo ${result},1); else result=$(echo ${result},0); fi
 if [ ! -f ${folder}index.html ]; then result=$(echo ${result},1); else result=$(echo ${result},0); fi 
 if [ ! -f ${folder}volume.nrrd ]; then result=$(echo ${result},1); else result=$(echo ${result},0); fi 
-echo ${result} | replace ',' $'\t' >> missingFiles.tsv
+echo ${result} | replace ',' $'\t' >> history/fullFilesList.tsv
 done
+cat history/fullFilesList.tsv | grep $'\t''1' >> history/missingFiles.tsv
 
