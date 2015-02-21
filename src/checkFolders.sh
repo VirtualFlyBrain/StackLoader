74dadd41a882adfe846b551c8be6df8813971e12
Jenkins CI Server <jenkins@karenin.inf.ed.ac.uk>
2015-02-21 21:18:48 +0000

    running folder checks

---
+++ src/checkFolders.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Checks all image folders and logs where a file type is missing or broken into missingFiles.tsv
+echo 'Checking files for...'
+echo reference$'\t'data.jso$'\t'thumbnail.png$'\t'index.html$'\t'volume.nrrd > missingFiles.tsv
+for folder in /partition/karenin/VFB/IMAGE_DATA/VFB/i/*/*/
+do 
+ref=$(echo ${folder} | replace '/' '' | replace 'partitionkareninVFBIMAGE_DATAVFBi' 'VFB_')$'\t'
+result=${ref}
+echo $ref
+if [ ! -f ${folder}data.jso ]; then result=$(echo ${result}$'\t'1); else result=$(echo ${result}$'\t'0); fi 
+if [ ! -f ${folder}thumbnail.png ]; then result=$(echo ${result}$'\t'1); else result=$(echo ${result}$'\t'0); fi
+if [ ! -f ${folder}index.html ]; then result=$(echo ${result}$'\t'1); else result=$(echo ${result}$'\t'0); fi 
+if [ ! -f ${folder}volume.nrrd ]; then result=$(echo ${result}$'\t'1); else result=$(echo ${result}$'\t'0); fi 
+echo ${result} >> missingFiles.tsv
+done
