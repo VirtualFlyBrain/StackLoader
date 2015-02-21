7ea3351332ab6ea3689760d8951315870743b7fe
Robert Court <robbie.court@gmail.com>
2015-02-21 13:52:45 +0000

    Adding auto brightest slice default view
    Ref #45 for all new datasets

--- ThenLoadWithJFRCtemplate.sh
+++ ThenLoadWithJFRCtemplate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Run this script to convert and load files listed in linkData.tsv to /disk/data/VFB/IMAGE_DATA/VFB... 
+# Run this script to convert and load files listed in linkData.tsv to /disk/data/VFB/IMAGE_DATA/VFB...
 # Note: All files will be removed after processing.
 # Then apply these stacks against the JFRC adult main brain template (001)
 export template=001
 export voxelSize='-x0.622088 -y0.622088 -z0.622088'
+export zSlice='0.622088'
 ./src/convert.sh
 ./src/setTemplate.sh
--- src/setTemplate.sh
+++ src/setTemplate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Set loaded files against the: 
+# Set loaded files against the:
 # 001 - JFRC main brain
 # 002 - Ito Half Brain
-# 003 - Court Adult VNS 
-# 004 - Wood Larval Brain 
-# template.  
+# 003 - Court Adult VNS
+# 004 - Wood Larval Brain
+# template.
 if [ ! -f ./linkData.tsv ]
 then
   echo "linkData.tsv file is missing! See linkData.tsv for an example"
@@ -12,6 +12,7 @@ else
   export imageDir='/disk/data/VFB/IMAGE_DATA/VFB/i/'
   export templateDir='/disk/data/VFB/IMAGE_DATA/VFB/t/'${template}'/'
   export jsoTemplate='templateForImage.jso'
+  source /partition/bocian/VFBTools/python-modules-2.6/bin/activate
   cat linkData.tsv | while IFS=$'\t' read -ra VFBI
   do
     label=${VFBI[0]}
@@ -20,9 +21,13 @@ else
     then
       last=$(echo ${ref} | replace 'VFB_' '' | cut -c 5-)
       first=$(echo ${ref} | replace 'VFB_' '' |  cut -c 1-4)
-      cat ${templateDir}${jsoTemplate} | replace 'FFFF' ${first} | replace 'LLLL' ${last} > ${imageDir}${first}/${last}/data.jso 
+      sliceNum=$(python /partition/bocian/VFBTools/NRRDtools/sliceNumber.py ${imageDir}${first}/${last}/volume.nrrd)
+      offset=$(echo ${sliceNum}*${zSlice}/2 | bc)
+      sliceMax=$(python /partition/bocian/VFBTools/NRRDtools/brightestSlice.py ${imageDir}${first}/${last}/volume.nrrd)
+      dist=$(echo ${sliceMax}*${zSlice}-${offset} | bc | awk '{printf("%d\n",$1 + 0.5)}')
+      cat ${templateDir}${jsoTemplate} | replace 'FFFF' ${first} | replace 'LLLL' ${last} | replace '"distance":"0"' '"distance":"'${dist}'"' > ${imageDir}${first}/${last}/data.jso
       echo Created json data file for $ref
-      echo '<html><head><meta HTTP-EQUIV="REFRESH" content="0; url=/site/tools/view_stack/3rdPartyStack.htm?tpbid='${ref}'"></head></html>' > ${imageDir}${first}/${last}/index.html 
+      echo '<html><head><meta HTTP-EQUIV="REFRESH" content="0; url=/site/tools/view_stack/3rdPartyStack.htm?tpbid='${ref}'"></head></html>' > ${imageDir}${first}/${last}/index.html
       echo Created html link file for $ref
     else
       echo WARNING: Not processing $ref, $label
@@ -36,4 +41,3 @@ else
   echo .
   echo ------------------------------------------------------
 fi
-
