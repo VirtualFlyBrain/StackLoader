15fcb9056fea768beb111fd4ac9d4d0970fea5a4
Robbie1977 <robbie.court@gmail.com>
2015-02-23 18:53:24 +0000

    Adding Jenkins console log messages

--- src/checkFolders.sh
+++ src/checkFolders.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Checks all image folders and logs where a file type is missing or broken into missingFiles.tsv
+echo 'Checking files for...' 1>&3
 echo 'Checking files for...'
 echo reference$'\t'data.jso$'\t'thumbnail.png$'\t'index.html$'\t'volume.nrrd > history/missingFiles.tsv
 echo reference$'\t'data.jso$'\t'thumbnail.png$'\t'index.html$'\t'volume.nrrd > history/fullFilesList.tsv
@@ -8,6 +9,7 @@ do
 ref=$(echo ${folder} | replace '/' '' | replace 'partitionkareninVFBIMAGE_DATAVFBi' 'VFB_')$'\t'
 result=${ref}
 echo $ref
+echo $ref 1>&3
 if [ ! -f ${folder}data.jso ]; then result=$(echo ${result},1); else result=$(echo ${result},0); fi 
 if [ ! -f ${folder}thumbnail.png ]; then result=$(echo ${result},1); else result=$(echo ${result},0); fi
 if [ ! -f ${folder}index.html ]; then result=$(echo ${result},1); else result=$(echo ${result},0); fi 
