526eac00a5e2486656029abcf136ec74f2dde10f
Rob Court <rcourt@ed.ac.uk>
2018-08-27 14:07:50 +0100

    Update ThenLoadWithLarvalL1template.sh

--- ThenLoadWithLarvalL1template.sh
+++ ThenLoadWithLarvalL1template.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run this script to convert and load files listed in linkData.tsv to /disk/data/VFB/IMAGE_DATA/VFB...
 # Note: All files will be removed after processing.
-# Then apply these stacks against David Wood's Larval L3 light level template (003)
+# Then apply these stacks against CATMAID Larval L1 light level template (005)
 export template=005
-export voxelSize='-x0.29 -y0.29 -z0.5'
-export zSlice='0.5'
+export voxelSize='-x380.0 -y380.0 -z50.0'
+export zSlice='50.0'
 ./src/convert.sh
 ./src/setTemplate.sh
